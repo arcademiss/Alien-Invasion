a67c95e7a3cecca34ad2103df0c3257e1ffce4a9
arcademiss <georgeventel97@gmail.com>
2022-01-06 17:44:40 +0200

    Adding the Alien class

--- alien.py
+++ alien.py
@@ -1,2 +1,21 @@
 import pygame
 from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """A class describing an alien"""
+    def __init__(self, ai_game):
+        """Initialises an instance of alien"""
+        super().__init__()
+        self.screen = ai_game.screen
+
+        # Load the image of alien
+        self.image = pygame.image.load('image/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Spawn an alien at the top of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the aliens horizontal position with precision
+        self.x = float(self.rect.x)
