ef067f94ab7007092ec4a9e9f123291ccc2bee00
arcademiss <georgeventel97@gmail.com>
2024-02-22 21:51:18 +0200

    Fixed High score crash

--- game_stats.py
+++ game_stats.py
@@ -8,9 +8,11 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
         self.active_game = False
-        self.high_score= 0
-        with open("high_score.json", 'r') as f:
-            self.high_score = json.load(f)
+        try:
+            with open("high_score.json", 'r') as f:
+                self.high_score = json.load(f)
+        except FileNotFoundError:
+            self.high_score = 0
 
     def reset_stats(self):
         """Initialises statistics that can change during game"""
